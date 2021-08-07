885833dbddfd5a1d3039990d63db133265888846
Siar <siar.wahidi@gmail.com>
2021-08-07 19:26:07 -0400

    Figured out how to create a function that generates divs :)

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-let divs = document.createElement('div');
+let container = document.querySelector('#container');
+
+let div = document.createElement('div');
+div.className = 'divs';
+container.appendChild(div);
+
+function createDiv() {
+    let div1 = document.createElement('div');
+    div1.className = 'divs';
+    container.appendChild(div1);
+
+    for (let i = 0; i < 100; i++) {
+        let div2 = document.createElement('div');
+        div2.className = 'divs';
+        container.appendChild(div2);
+    }
+}
+
+createDiv();
+
+
 
