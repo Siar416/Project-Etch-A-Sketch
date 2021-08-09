0995792e5925e06595f9baa60f042af3ca4a3bdd
Siar <siar.wahidi@gmail.com>
2021-08-09 18:39:01 -0400

    added on mouse enter feature to change color to grey

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let container = document.querySelector('#container');
+let allDivs = document.getElementsByClassName('divs');
 
 function createDiv() {
 
-    for (let i = 0; i < 684; i++) {
+    for (let i = 0; i < 1406; i++) {
         let div2 = document.createElement('div');
         container.appendChild(div2);
             div2.className = 'divs';
             container.appendChild(div2);
         }
-        for (let j = 0; j < 684; j++) {
+        for (let j = 0; j < 1406; j++) {
             let div3 = document.createElement('div');
             container.appendChild(div3);
                 div3.className = 'divs';
@@ -17,5 +18,8 @@ function createDiv() {
     }
 createDiv();
 
-let allDivs = document.getElementsByClassName('divs');
-
+for(let i = 0; i < allDivs.length; i++) {
+    allDivs[i].onmouseenter = function(e) {
+        e.target.style.backgroundColor = 'grey';
+    }
+}
